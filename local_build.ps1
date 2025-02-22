346873e2d7568693bb9e7503383c2cde16a4ad78
Alejoide <alejofsarmiento@gmail.com>
2025-02-21 23:40:17 -0300

    fix: add copy command for updater executable to src for development testing

--- local_build.ps1
+++ local_build.ps1
@@ -1,4 +1,5 @@
 $env:DEV_MODE = "False"
 & poetry run pyinstaller .\updater.spec --noconfirm
-& mv .\dist\updater.exe .\ -Force
+& mv .\dist\updater.exe .\ -Force # Para el build final
+& cp .\updater.exe .\src -Force # Para pruebas en desarrollo
 & poetry run pyinstaller .\budget-maker-desktop.spec --noconfirm
