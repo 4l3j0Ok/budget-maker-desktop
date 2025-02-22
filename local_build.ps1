990d93cb9b4a956007b85adfefd425a6e4c3719e
Alejoide <alejofsarmiento@gmail.com>
2025-02-21 23:04:01 -0300

    fix: add --noconfirm option to pyinstaller commands in local build script

--- local_build.ps1
+++ local_build.ps1
@@ -1,4 +1,4 @@
 $env:DEV_MODE = "False"
-& poetry run pyinstaller .\updater.spec
+& poetry run pyinstaller .\updater.spec --noconfirm
 & mv .\dist\updater.exe .\ -Force
-& poetry run pyinstaller .\budget-maker-desktop.spec
+& poetry run pyinstaller .\budget-maker-desktop.spec --noconfirm
